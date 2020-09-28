00d66f22335448da03fe54a1727f4f42e0ad8806
mblanco6149 <manu.blanco6149@gmail.com>
2020-09-28 13:12:49 -0300

    Initialize fachertiApp

---
+++ facheritApp/src/test/java/com/grupo9/facheritApp/FacheritAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo9.facheritApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacheritAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
