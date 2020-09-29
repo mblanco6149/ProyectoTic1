2218bd5e1282d6c75183a11e3b60673996a0d8fc
ssouberbielle <ssouberbielle@hotmail.com>
2020-09-29 19:10:47 -0300

    Agregue porp de Client

--- facheritApp/src/main/java/tic1/grupo9/facheritApp/backend/appi/BackendServiceImp.java
+++ facheritApp/src/main/java/tic1/grupo9/facheritApp/backend/appi/BackendServiceImp.java
@@ -3,4 +3,13 @@ package tic1.grupo9.facheritApp.backend.appi;
 import tic1.grupo9.facheritApp.commons.appi.BackendService;
 
 public class BackendServiceImp implements BackendService {
+    @Override
+    public void saveNewBrand() {
+
+    }
+
+    @Override
+    public void saveAdmin() {
+
+    }
 }
--- facheritApp/src/main/java/tic1/grupo9/facheritApp/commons/entities/Client.java
+++ facheritApp/src/main/java/tic1/grupo9/facheritApp/commons/entities/Client.java
@@ -1,4 +1,37 @@
 package tic1.grupo9.facheritApp.commons.entities;
 
-public class Client {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Client extends User {
+
+    @Column
+    protected String name;
+
+    @Column
+    protected String surname;
+
+    @Column
+    protected long phone;
+
+    @Column
+    protected String direccion;
+
+    public Client(String email, String password, String name, String surname, long phone, String direccion)
+    {
+        super(email, password);
+        this.name=name;
+        this.surname=surname;
+        this.phone=phone;
+        this.direccion=direccion;
+    }
+
 }
