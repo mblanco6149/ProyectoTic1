d25241f4c2f162b109a025fe0a4445b8add20a0e
mblanco6149 <manu.blanco6149@gmail.com>
2020-10-28 16:27:53 -0300

    Actualice StartAppiController

--- facheritApp/src/main/java/tic1/grupo9/facheritApp/frontend/controllers/StartAppiController.java
+++ facheritApp/src/main/java/tic1/grupo9/facheritApp/frontend/controllers/StartAppiController.java
@@ -10,14 +10,12 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Menu;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -59,7 +57,7 @@ public class StartAppiController implements Initializable {
     @FXML
     private GridPane grid;
 
-    
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -86,7 +84,7 @@ public class StartAppiController implements Initializable {
                     ImageView image1 = clothTemp.getPicture();
 
 
-                    TextArea textArea = new TextArea();
+                    Label textArea = new Label();
                     textArea.setText(cls.getByLastId().toString());
                     content.getChildren().add(image1);
                     content.getChildren().add(textArea);
