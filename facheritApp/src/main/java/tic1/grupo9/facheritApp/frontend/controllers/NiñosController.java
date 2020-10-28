dcc94f15c660a75feb9327b65eaabfb4028573ea
mblanco6149 <manu.blanco6149@gmail.com>
2020-10-28 20:20:57 -0300

    Actualice mujerController y hombre.fxml

--- facheritApp/src/main/java/tic1/grupo9/facheritApp/frontend/controllers/NiñosController.java
+++ facheritApp/src/main/java/tic1/grupo9/facheritApp/frontend/controllers/NiñosController.java
@@ -1,4 +1,19 @@
 package tic1.grupo9.facheritApp.frontend.controllers;
 
-public class NiñosController {
+import javafx.fxml.Initializable;
+import org.springframework.stereotype.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Controller
+public class NiñosController  implements Initializable {
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
