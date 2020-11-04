2fd5c82824bf51d3823fda40f6225a8281cc7cc1
ssouberbielle <ssouberbielle@hotmail.com>
2020-11-03 21:41:08 -0300

    Agrego fxml de admin con botones crear local, marca y stock, creo controladores pra cada uno.

---
+++ facheritApp/src/main/java/tic1/grupo9/facheritApp/frontend/controllers/BrandController.java
@@ -0,0 +1,21 @@
+package tic1.grupo9.facheritApp.frontend.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import org.springframework.stereotype.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Controller
+public class BrandController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void create(javafx.event.ActionEvent actionEvent){
+
+    }
+}
---
+++ facheritApp/src/main/java/tic1/grupo9/facheritApp/frontend/controllers/StockController.java
@@ -0,0 +1,20 @@
+package tic1.grupo9.facheritApp.frontend.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import org.springframework.stereotype.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Controller
+public class StockController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void add(javafx.event.ActionEvent actionEvent){}
+}
